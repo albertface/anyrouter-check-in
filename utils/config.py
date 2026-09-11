5291e793bdcf301c54cfda0389b725ef52d360ed
Lei Zhang <marshal.yuanshuai@outlook.com>
2026-09-11 15:41:45 +0800

    Update agentrouter domain and proxy settings

--- utils/config.py
+++ utils/config.py
@@ -96,14 +96,14 @@ class AppConfig:
 			),
 			'agentrouter': ProviderConfig(
 				name='agentrouter',
-				domain='https://ps.air-outer.com',
+				domain='https://agentrouter.org',
 				login_path='/login',
 				sign_in_path=None,  # 无需签到接口，查询用户信息时自动完成签到
 				user_info_path='/api/user/self',
 				api_user_key='new-api-user',
 				bypass_method='waf_cookies',
 				waf_cookie_names=['acw_tc'],
-				use_proxy=False,
+				use_proxy=True,
 				persist_profile=False,
 			),
 		}
